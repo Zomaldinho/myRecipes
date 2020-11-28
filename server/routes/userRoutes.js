14415374ac2eed5b2beae0bc94e87ee463b74613
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2020-11-28 16:13:17 +0200

    updating users routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,6 +5,10 @@ const router = express.Router();
 
 router.post('/register', (req, res) => {
   let { name, email, password } = req.body;
+  let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (!name || !email || !password || !email.match(mailformat)) {
+    return res.status(400).json('Wrong Inputs');
+  }
   let user = new User({
     name,
     email,
@@ -12,8 +16,37 @@ router.post('/register', (req, res) => {
   });
   user
     .save()
-    .then((data) => res.json(data))
+    .then((user) =>
+      res.json({
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        date: user.date,
+      })
+    )
     .catch(console.log);
 });
 
-module.exports = router
+app.post('/signin', (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json('Empty Inputs');
+  }
+  User.findOne({ email: email })
+    .then((user) => {
+      const validPass = bcrypt.compareSync(password, user.password);
+      if (validPass) {
+        return res.json({
+          id: user._id,
+          name: user.name,
+          email: user.email,
+          date: user.date,
+        });
+      } else {
+        return res.status(400).json('Wrong Credentials');
+      }
+    })
+    .catch((err) => res.status(400).json('Unable to get the user'));
+});
+
+module.exports = router;
