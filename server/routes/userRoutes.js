dc78246561c4283c87931053443a3a77bb419696
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2020-11-28 16:05:22 +0200

    updating users routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,19 @@
-const express = require('express')
-const User = require('../models/users')
-const router = express.Router()
+const express = require('express');
+const bcrybt = require('bcryptjs');
+const User = require('../models/users');
+const router = express.Router();
 
-router.get('/register',(req,res)=>{
-  
-})
+router.post('/register', (req, res) => {
+  let { name, email, password } = req.body;
+  let user = new User({
+    name,
+    email,
+    password: bcrybt.hashSync(password, 12),
+  });
+  user
+    .save()
+    .then((data) => res.json(data))
+    .catch(console.log);
+});
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 require('./db/connect')
+const usersRoutes = require('./routes/userRoutes')
 
 const app = express();
 app.use(express.json())
+app.use(usersRoutes)
+
 port = process.env.PORT || 5000
 
 app.listen(port, ()=> console.log(`App is running on port ${port}`))
